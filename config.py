eded5e4c528fc3b84d3842e0da9792860e7e1d42
kimcorrectcar-lgtm <kimcorrectcar@gmail.com>
2026-08-20 15:18:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,21 +2,13 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+PORT = int(os.getenv("PORT", "10000"))
 
-BOT_TOKEN = os.getenv(
-    "BOT_TOKEN"
-)
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
-)
-
-PORT = int(
-    os.getenv(
-        "PORT",
-        "10000",
-    )
-)
+MANAGER_PHONE = os.getenv("MANAGER_PHONE", "+7XXXXXXXXXX")
+MANAGER_TELEGRAM = os.getenv("MANAGER_TELEGRAM", "")
+MANAGER_WHATSAPP = os.getenv("MANAGER_WHATSAPP", "")
+MANAGER_MAX_LEADS_CHAT_ID = os.getenv("MANAGER_MAX_LEADS_CHAT_ID", "")
